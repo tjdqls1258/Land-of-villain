47fa2a85e4e3ba1fb2ae80724e0d8a8ad9ffb052
20171119kim <skyksk981205@gmail.com>
2021-06-29 01:02:43 +0900

    아이템/0번째/21.06.29
    아이템/0번째/21.06.29

---
+++ Unity Project/Assets/Item/Item.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{   //Normal 아이템 이름.(일반적인 아이템)
+    string[] N_Weapon_Item = new string[] { "Copper_Sword" };
+   
+
+    string[] N_Armor_Item = new string[] { "Copper_Armor" };
+
+    string[] N_Hat_Item = new string[] { "Copper_Hat" };
+
+    string[] N_Accessory_Item = new string[] { "Copper_Ring" };
+    public string get_N_Item()
+    {
+        int N1 = Random.Range(0, 4);//장비의 종류를 결정해줌.
+        int N2; //아이템의 총 개수에서 하나를 가져오기 위한 변수. 
+        string N3="";//아이템을 이름을 지닐 변수
+        if (N1 == 0)
+        {
+            N2 = Random.Range(0, N_Weapon_Item.Length);
+            N3= N_Weapon_Item[N2];
+        }
+        else if (N1 == 1)
+        {
+            N2 = Random.Range(0, N_Armor_Item.Length);
+            N3 = N_Armor_Item[N2];
+        }
+        else if (N1 == 2)
+        {
+            N2 = Random.Range(0, N_Hat_Item.Length);
+            N3 = N_Hat_Item[N2];
+        }
+        else if (N1 == 3)
+        {
+            N2 = Random.Range(0, N_Accessory_Item.Length);
+            N3 = N_Accessory_Item[N2];
+        }
+
+        return N3;
+    }
+    //Rare 아이템 이름.(좋은 아이템)
+
+    string[] R_Weapon_Item = new string[] { "Silver_Sword" };
+
+    string[] R_Armor_Item = new string[] { "Silver_Armor" };
+
+    string[] R_Hat_Item = new string[] { "Silver_Hat" };
+
+    string[] R_Accessory_Item = new string[] { "Silver_Ring" };
+
+    public string get_R_Item()
+    {
+        int N1 = Random.Range(0, 4);//장비의 종류를 결정해줌.
+        int N2; //아이템의 총 개수에서 하나를 가져오기 위한 변수. 
+        string N3 = "";//아이템을 이름을 지닐 변수
+        if (N1 == 0)
+        {
+            N2 = Random.Range(0, R_Weapon_Item.Length);
+            N3 = R_Weapon_Item[N2];
+        }
+        else if (N1 == 1)
+        {
+            N2 = Random.Range(0, R_Armor_Item.Length);
+            N3 = R_Armor_Item[N2];
+        }
+        else if (N1 == 2)
+        {
+            N2 = Random.Range(0, R_Hat_Item.Length);
+            N3 = R_Hat_Item[N2];
+        }
+        else if (N1 == 3)
+        {
+            N2 = Random.Range(0, R_Accessory_Item.Length);
+            N3 = R_Accessory_Item[N2];
+        }
+
+        return N3;
+    }
+
+    //Epic 아이템 이름.(희귀한 아이템)
+
+    string[] E_Weapon_Item = new string[] { "Gold_Sword" };
+
+    string[] E_Armor_Item = new string[] { "Gold_Armor" };
+
+    string[] E_Hat_Item = new string[] { "Gold_Hat" };
+
+    string[] E_Accessory_Item = new string[] { "Gold_Ring" };
+    public string get_E_Item()
+    {
+        int N1 = Random.Range(0, 4);//장비의 종류를 결정해줌.
+        int N2; //아이템의 총 개수에서 하나를 가져오기 위한 변수. 
+        string N3 = "";//아이템을 이름을 지닐 변수
+        if (N1 == 0)
+        {
+            N2 = Random.Range(0, E_Weapon_Item.Length);
+            N3 = E_Weapon_Item[N2];
+        }
+        else if (N1 == 1)
+        {
+            N2 = Random.Range(0, E_Armor_Item.Length);
+            N3 = E_Armor_Item[N2];
+        }
+        else if (N1 == 2)
+        {
+            N2 = Random.Range(0, E_Hat_Item.Length);
+            N3 = E_Hat_Item[N2];
+        }
+        else if (N1 == 3)
+        {
+            N2 = Random.Range(0, E_Accessory_Item.Length);
+            N3 = E_Accessory_Item[N2];
+        }
+
+        return N3;
+    }
+}

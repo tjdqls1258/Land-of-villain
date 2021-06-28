f9354a732bb67ec1aaad60d902d1c3b1657e25b1
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-06-28 11:30:15 +0900

    Monster

---
+++ Unity Project/Assets/Monster/Move_monster.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_monster : MonoBehaviour
+{
+    [SerializeField]
+    private float rotateSpeed;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float Hp;
+
+    private Rigidbody2D rigid;
+    private GameObject Player;
+    private Transform transform;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        transform = GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAt_Player();
+        move();
+    }
+
+    void LookAt_Player()//플레이어를 바라보게 하는 함수
+    {
+        Vector2 vec = Vector2.zero;//초기화
+        if (transform != null)
+        {
+            vec = new Vector2(Player.transform.position.x - transform.position.x,
+                Player.transform.position.y - transform.position.y);
+            //트렌스폼 찾으면 플레이어를 바라보는 벡터구함.
+        }
+
+        float angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg; //y와 x의 좌표를 탄젠트해서 각도를 구함
+        Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward); //회전할 Z축 각도 저장
+        Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, rotateSpeed * Time.deltaTime);
+        //회전 속도
+
+        transform.rotation = rotation; // 회전
+    }
+
+    void move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, moveSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Attack")
+        {
+            
+        }
+    }
+}

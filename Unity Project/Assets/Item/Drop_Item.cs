c3e67aa7ead391daf579cbecb5d0dda3540a39b6
20171119kim <skyksk981205@gmail.com>
2021-06-29 23:50:09 +0900

    김성겸/21.06.29/Item관련 수정,Player_State제작,Player_State_Prefab제작
    Item관련
    drop_Item: 코드 분리하여 정리
    Item: 다중배열 사용하는 코드로 변경
    ===========================
    Player_State관련
    Player_stat: 내용 수정
    Player_State_Window : Player_Stat를 가진 오브젝트에서 Player_Stat를 받아와 텍스트 변경으로 능력치를 볼 수 있게 제작
    ====================
    State_Prefap관련
    State_Prefap: UI폴더 안에 들어있는 State_Prefap의 폴더 안의 Carvas 하위 오브젝트에 플레이어 스테이터스를 보여주는 TextUI를 넣음.
     Player_State_Window 컴포넌트를 지니고 있음.

--- Unity Project/Assets/Item/Drop_Item.cs
+++ Unity Project/Assets/Item/Drop_Item.cs
@@ -9,41 +9,53 @@ public class Drop_Item
     //랜덤한 아이템의 이름을 반환하는 함수.
     //get_Place: 아이템이 나오는 장소(?) [희귀 몬스터=0, 보스 몬스터=1, 보물방=2] 
     public string drop_Item(int get_Place)
-    {
-        string get_Item_Name="";
+    { 
  
         switch (get_Place)
         {   //일단은 아무것도 주지 않는 것은 구현안함 만들고 좀 생각해봐야 할 듯
             case 0:
-                int N1 = Random.Range(0, 10);//일반 아이템을 줄지 좋은 아이템을 줄지 결정하는 코드 7(Nomal):3(Rare)
-                if (N1 >= 6)
-                {
-                    get_Item_Name= item_Name.get_N_Item();
-
-                }
-                else if (N1 <= 7)
-                { get_Item_Name= item_Name.get_R_Item(); }
-                break;
+                return Get_Rare_Monster();
             case 1:
-                int N2 = Random.Range(0, 10);//좋은 아이템을 줄지 희귀한 아이템을 줄지 결정하는 코드 7(Rare) : 3(Epic) 
-                if (N2 >= 6)
-                { get_Item_Name= item_Name.get_R_Item(); }
-                else if (N2 <= 7)
-                { get_Item_Name= item_Name.get_E_Item(); }
-                break;
-            case 2://일반,좋은,희귀한 아이템을 줄지 결정하는 코드 2(Namal):4(Rare):4(Epic)
-                int N3 = Random.Range(0, 10);
-                if (N3 >= 0 && N3 <= 2)
-                { get_Item_Name= item_Name.get_N_Item(); }
-                else if (N3 >= 3 && N3 <= 6)
-                { get_Item_Name= item_Name.get_R_Item(); }
-                else if (N3 >= 7 && N3 <= 9)
-                { get_Item_Name= item_Name.get_E_Item(); }
-                break;
-
-
+                return Get_Boss_Monster();
+            case 2:
+                return Get_TreasureRoom_Monster();
+            default:
+                return "NONE";
         }
 
-        return get_Item_Name;
+    }
+    public string Get_Rare_Monster()
+    {
+        int N1 = Random.Range(0, 10);//일반 아이템을 줄지 좋은 아이템을 줄지 결정하는 코드 7(Nomal):3(Rare)
+        if (N1 >= 6)
+        {return item_Name.get_N_Item();}
+        else if (N1 <= 7)
+        { return item_Name.get_R_Item(); }
+        else
+        {return "NONE";}
+    }
+
+    public string Get_Boss_Monster()
+    {
+        int N1 = Random.Range(0, 10);//좋은 아이템을 줄지 희귀한 아이템을 줄지 결정하는 코드 7(Rare) : 3(Epic) 
+        if (N1 >= 6)
+        { return item_Name.get_R_Item(); }
+        else if (N1 <= 7)
+        { return item_Name.get_E_Item(); }
+        else { return "NONE"; }
+    }
+
+    public string Get_TreasureRoom_Monster()
+    {
+        int N1 = Random.Range(0, 10);//일반,좋은,희귀한 아이템을 줄지 결정하는 코드 2(Namal):4(Rare):4(Epic)
+        if (N1 >= 0 && N1 <= 2)
+        { return item_Name.get_N_Item(); }
+        else if (N1 >= 3 && N1 <= 6)
+        { return item_Name.get_R_Item(); }
+        else if (N1 >= 7 && N1 <= 9)
+        { return item_Name.get_E_Item(); }
+        else { return "NONE"; }
     }
 }
+
+
---
+++ Unity Project/Assets/State/Player_Stat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Stat : MonoBehaviour
+{
+    /*
+    int MaxHP=100; //최대 체력[0]
+    int HP=100; //체력[1]
+    int ATK=10; //공격력[2]
+    int DEF=5; //방어력[3]
+    int AGI=5; //민첩성(이동속도를 담당(추후에 가능하면 공격속도도?))[4]
+    int LUC=0; //운(치명타)[5]
+    int Money=0; //소지금[6]
+    */
+    //위에 것같은 스탯을 배열로 다룸
+    private int[] P_State = new int[] { 100,100,10,5,5,0,0 };
+    
+    public void Set_P_State(int N1,int N2)
+    { P_State[N1] = N2; }
+    public int Get_P_State(int N1)
+    {
+        return P_State[N1];
+    }
+}

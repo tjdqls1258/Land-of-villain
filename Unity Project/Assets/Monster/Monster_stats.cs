c11d69b175cc740a0db6c1d9cff061eba0568727
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2021-06-30 18:12:32 +0900

    스테이지 클리어 중간과정

---
+++ Unity Project/Assets/Monster/Monster_stats.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Monster_stats : MonoBehaviour
+{
+    public int Hp;
+    public int damage;
+    public float Atk_dir;
+    Drop_Item item_Drop;
+    private string Drop_item_it;
+
+    public int Monster_Drop_Tear;
+
+    public GameObject stageManger;
+
+    private void Awake()
+    {
+        stageManger = GameObject.Find("StageManager");
+        item_Drop = new Drop_Item();
+    }
+
+    void Get_damange(int damage)
+    {
+        
+        Hp -= damage;
+        if (Hp <= 0) //체력 0 되면 사망
+        {
+            die();
+        }
+    }
+    void die()
+    {
+        Drop_item_it = item_Drop.drop_Item(Monster_Drop_Tear);
+        GameObject item = (GameObject)Resources.Load("Item/" + Drop_item_it); 
+        //리소스파일 속 item파일에 있는 Drop_item_it와 같은 이름을 가진 프리팹
+        if (item != null) //있으면 소환 없으면 아무것도 안함
+        {
+            Instantiate(item, this.transform);
+        }
+        stageManger.GetComponent<StageManager>().monsterdead();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player_ATk") //플레이어(임시 스킬로 대체할 예정)와 충돌시
+        {
+            //충돌한 객체의 컴퍼넌트에서 데미지 받아옴
+            Get_damange(other.GetComponent<Player_bullet>().Damage());
+            
+            if (Hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+
+    public float give_damage()
+    {
+        return damage;
+    }
+}
---
+++ Unity Project/Assets/Scenes/StageManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageManager : MonoBehaviour
+{
+    public GameObject Player;
+    //public GameObject[] Monster;
+    public List<GameObject> FoundObjects;
+    public string stage;
+
+    private int monsternum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FoundObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag("Monster"));
+        monsternum = FoundObjects.Count;
+        //Monster = GameObject.FindGameObjectsWithTag("Monster");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(monsternum <= 0)
+        {
+            SceneManager.LoadScene(stage);
+            SceneManager.MoveGameObjectToScene(Player, SceneManager.GetSceneByName(stage));
+        }
+    }
+
+    public void monsterdead()
+    {
+        monsternum -= 1;
+    }
+}

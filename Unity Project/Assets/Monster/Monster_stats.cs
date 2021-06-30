1e11598bad227d78f2b349ed4ea2b1e8b44525f0
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2021-06-30 18:26:24 +0900

    스테이지 클리어 중간과정

--- Unity Project/Assets/Monster/Monster_stats.cs
+++ Unity Project/Assets/Monster/Monster_stats.cs
@@ -13,14 +13,17 @@ public class Monster_stats : MonoBehaviour
 
     public int Monster_Drop_Tear;
 
+    public GameObject stageManger;
+
     private void Awake()
     {
+        stageManger = GameObject.Find("StageManager");
         item_Drop = new Drop_Item();
     }
 
     void Get_damange(int damage)
     {
-        
+
         Hp -= damage;
         if (Hp <= 0) //체력 0 되면 사망
         {
@@ -30,12 +33,13 @@ public class Monster_stats : MonoBehaviour
     void die()
     {
         Drop_item_it = item_Drop.drop_Item(Monster_Drop_Tear);
-        GameObject item = (GameObject)Resources.Load("Item/" + Drop_item_it); 
+        GameObject item = (GameObject)Resources.Load("Item/" + Drop_item_it);
         //리소스파일 속 item파일에 있는 Drop_item_it와 같은 이름을 가진 프리팹
         if (item != null) //있으면 소환 없으면 아무것도 안함
         {
             Instantiate(item, this.transform);
         }
+        stageManger.GetComponent<StageManager>().monsterdead();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -43,7 +47,7 @@ public class Monster_stats : MonoBehaviour
         {
             //충돌한 객체의 컴퍼넌트에서 데미지 받아옴
             Get_damange(other.GetComponent<Player_bullet>().Damage());
-            
+
             if (Hp <= 0)
             {
                 Destroy(gameObject);

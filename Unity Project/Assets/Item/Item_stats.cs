2efc1d45097877a4c8f0b5a31ab424e5b9c90264
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-06-30 15:17:40 +0900

    아이템

---
+++ Unity Project/Assets/Item/Item_stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_stats : MonoBehaviour
+{
+    public int[] Item_stat = { }; //종류,(무기일경우) 공격력, 방어력, 부가 스텟
+
+    private void Awake()
+    {
+        
+    }
+}
---
+++ Unity Project/Assets/Player/Player_Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Item : MonoBehaviour
+{
+    string[] Items;
+    Item item;
+    GameObject Waapon, Armor, Ring, Hat;
+    void Awake()
+    {
+        item = new Item();
+        
+        Waapon = (GameObject)Resources.Load("Item/" + item.get_F_Item(0));
+        Armor = (GameObject)Resources.Load("Item/" + item.get_F_Item(1));
+        Hat = (GameObject)Resources.Load("Item/" + item.get_F_Item(2));
+        Ring = (GameObject)Resources.Load("Item/" + item.get_F_Item(3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

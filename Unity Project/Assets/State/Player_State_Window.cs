7c3eb17d0e2ca275e7c9069112a227511a19e1a2
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2021-07-11 23:14:39 +0900

    상태창 착용장비(텍스트만)

--- Unity Project/Assets/State/Player_State_Window.cs
+++ Unity Project/Assets/State/Player_State_Window.cs
@@ -7,6 +7,7 @@ public class Player_State_Window : MonoBehaviour
 {   //이 코드는 캔버스나, Player_State_Window_UI에 직접 넣는게 아닌 하나의 통합 UI에 넣는 코드임을 고려해서 작성함
     private Text P_S_Text;
     Player_Stat P_S;
+    Player_Item P_I;
 
     void Start()
     {
@@ -14,7 +15,7 @@ public class Player_State_Window : MonoBehaviour
         P_S_Text = GameObject.Find("Player_State_UI_text").GetComponent<Text>();
         //일단 플레이어라는 빈 오브젝트안에 Player_Stat 스크립을 넣음.
         P_S = GameObject.Find("Player").GetComponent<Player_Stat>();
-
+        P_I = GameObject.Find("Player").GetComponent<Player_Item>();
         //이 텍스트를 넣어줄 패널 필요
     }
 
@@ -31,6 +32,12 @@ public class Player_State_Window : MonoBehaviour
         + "<DEF> : " + P_S.Get_P_State(3) + "\n"
         + "<AGI> : " + P_S.Get_P_State(4) + "\n"
         + "<LUC> : " + P_S.Get_P_State(5) + "\n"
-        + "<Money> : " + P_S.Get_P_State(6); 
+        + "<Money> : " + P_S.Get_P_State(6) + "\n"
+        + "\n" + "\n"
+        + "equipment" + "\n"
+        + "weapon : " + P_I.Get_Player_Item(0) + "\n"
+        + "armor : " + P_I.Get_Player_Item(1) + "\n"
+        + "hat : " + P_I.Get_Player_Item(2) + "\n"
+        + "ring : " + P_I.Get_Player_Item(3) + "\n"; 
     }
 }

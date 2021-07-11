145a373a4755c55cb1fd5fca48472962544c8d25
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2021-07-11 17:23:45 +0900

    플레이어 상태창

---
+++ Unity Project/Assets/UI/Player_State_btn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_State_btn : MonoBehaviour
+{
+    [SerializeField] private GameObject Player_State;
+
+    public void P_State_btn()
+    {
+        Player_State.SetActive(true);
+        Time.timeScale = 0.0f;     
+    }
+
+    public void Back_btn()
+    {
+        Player_State.SetActive(false);
+        Time.timeScale = 0.0f;
+    }
+}

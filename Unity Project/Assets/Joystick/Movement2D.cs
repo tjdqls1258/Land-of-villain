f44a933254c68326bb2b3bd046a2753cda199302
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-07-12 16:11:10 +0900

    캐릭터 이동 애니메이션 추가

--- Unity Project/Assets/Joystick/Movement2D.cs
+++ Unity Project/Assets/Joystick/Movement2D.cs
@@ -7,7 +7,14 @@ public class Movement2D : MonoBehaviour
 {
     public float moveSpeed;                         // 이동속도
     private Vector3 moveDirection = Vector3.zero;   // 이동방향
+    Animator animator;
+    SpriteRenderer rend;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
         float x = Joystick.inputDirection.x;        // 좌우 이동
@@ -16,6 +23,62 @@ public class Movement2D : MonoBehaviour
         // 이동방향 설정
         moveDirection = new Vector3(x, y, 0);
 
+        
+        if(x>0 )
+        {
+            animator.SetBool("LR", true);
+            Debug.Log("오른쪽으로 이동");
+            animator.SetBool("Back", false);
+            animator.SetBool("Walk", false);
+            rend.flipX = false;
+            if (y < -0.4)
+            {
+                animator.SetBool("Walk", true);
+                animator.SetBool("Back", false);
+                animator.SetBool("LR", false);
+
+                Debug.Log("위로 이동");
+
+            }
+            else if (y > 0.4)
+            {
+                animator.SetBool("Back", true);
+                animator.SetBool("LR", false);
+                animator.SetBool("Walk", false);
+                Debug.Log("아래로 이동");
+            }
+            
+        }
+        if(x < 0)
+        {
+            animator.SetBool("LR", true);
+            animator.SetBool("Back", false);
+            animator.SetBool("Walk", false);
+            rend.flipX = true;
+            if (y < -0.4)
+            {
+                animator.SetBool("Walk", true);
+                animator.SetBool("Back", false);
+                animator.SetBool("LR", false);
+
+                Debug.Log("위로 이동");
+
+            }
+            else if (y > 0.4)
+            {
+                animator.SetBool("Back", true);
+                animator.SetBool("LR", false);
+                animator.SetBool("Walk", false);
+                Debug.Log("아래로 이동");
+            }
+            Debug.Log("왼쪽으로 이동");
+        }
+        if((x==0) &&(y==0))
+        {
+            animator.SetBool("Back", false);
+            animator.SetBool("LR", false);
+            animator.SetBool("Walk", false);
+        }
         // 새로운 위치 = 현재 위치 + (방향 * 속도)
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10.0f, 10.0f), Mathf.Clamp(transform.position.y, -10.0f, 10.0f), 0);

ab79b4157ccdadcc02429d17d8b06ececce5db40
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-07-12 20:24:15 +0900

    다 고침
    제발

--- Unity Project/Assets/Item/Get_Item/Player_Get_Item.cs
+++ Unity Project/Assets/Item/Get_Item/Player_Get_Item.cs
@@ -33,7 +33,7 @@ public class Player_Get_Item : MonoBehaviour
   
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.CompareTag("Item"))
+        if (coll.tag == "Item")
         {   //Item_stats 받아오기
             IS = coll.GetComponent<Item_stats>();
             //아이템 습득시 게임오브젝트 삭제를 위한 할당
@@ -41,7 +41,7 @@ public class Player_Get_Item : MonoBehaviour
             //충돌을 Drop_Item에서 확인 하기 위한 변수
             Item_Check = true;
         }
-        else if (coll.gameObject.CompareTag("Money"))
+        else if (coll.tag == "Money")
         {   //Money_Stats 받아오기
             MS = coll.GetComponent<Money_Stats>();
             //돈 습득시 게임오브젝트를 삭제를 위한 할당
@@ -66,7 +66,7 @@ public class Player_Get_Item : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D coll)
     {
-        if (coll.gameObject.CompareTag("Item"))
+        if (coll.tag == "Item")
         {   //초기화
             IS = null;
             DI = null;
@@ -96,13 +96,18 @@ public class Player_Get_Item : MonoBehaviour
             }
             else if (PI.Get_Player_Item(IS.Get_Item_Kind()) != "NONE")//플레이어가 아이템을 가지고 있었다면.
             {
-                //플레이어가 가지고 있던 아이템 프리팹을 생성
-                Creat_Drop_Item(PI.Get_Player_Item(IS.Get_Item_Kind()));
-                //플레이어의 아이템의 장비에 맞춰 플레이어 장비에 이름을 할당해줌.
-                PI.Set_Player_Item(IS.Get_Item_Kind(), IS.Get_Item_Name());
-                Destroy(DI.gameObject);//그리고 주운 아이템 파괴 처리
-
-
+                if (PI.아이템_강화(IS.Get_Item_Kind(), IS.Item_Name))
+                {
+                    Destroy(DI.gameObject);//그리고 주운 아이템 파괴 처리
+                }
+                else
+                {
+                    //플레이어가 가지고 있던 아이템 프리팹을 생성
+                    Creat_Drop_Item(PI.Get_Player_Item(IS.Get_Item_Kind()));
+                    //플레이어의 아이템의 장비에 맞춰 플레이어 장비에 이름을 할당해줌.
+                    PI.Set_Player_Item(IS.Get_Item_Kind(), IS.Get_Item_Name());
+                    Destroy(DI.gameObject);//그리고 주운 아이템 파괴 처리
+                }
             }
 
         }

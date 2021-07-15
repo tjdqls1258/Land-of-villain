47ffc10f55451561663b4f5555b7f772b0bb9520
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-07-15 23:10:48 +0900

    11스테이지 도달시 배경 변화

---
+++ Unity Project/Assets/SettingMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMap : MonoBehaviour
+{
+    Player_Stat player_state;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player_state = GameObject.Find("Player").GetComponent<Player_Stat>();
+        if (player_state.N_Stages >= 0)
+        {
+            transform.Find("Stage01").gameObject.SetActive(true);
+        }
+        if (player_state.N_Stages > 10)
+        {
+            transform.Find("Stage01").gameObject.SetActive(false);
+            transform.Find("Stage02").gameObject.SetActive(true);
+        }
+    }
+}

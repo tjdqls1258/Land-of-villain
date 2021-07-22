338e98ad3cc7260d4615cd2acf957bc29991d648
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-07-22 23:52:12 +0900

    버프칸 구현
    버프칸 구현

--- Unity Project/Assets/Resources/Skill/Skill_List/H_Skill_01.cs
+++ Unity Project/Assets/Resources/Skill/Skill_List/H_Skill_01.cs
@@ -6,9 +6,16 @@ public class H_Skill_01 : MonoBehaviour, Skill
 {
     public float 지속시간;
     int reset_stat;
+    public GameObject Buffe_Image;
+    GameObject Buffe_;
     public void Skill_Action()
     {
         GameObject Player = GameObject.Find("Player");
+
+        GameObject Buffe_Panel = Player.transform.Find("Play_UI").transform.
+            Find("BuffPanel").gameObject;
+        Buffe_ = Instantiate(Buffe_Image, Vector3.zero, Quaternion.identity);
+        Buffe_.transform.parent = Buffe_Panel.transform;
 
         reset_stat = Player.GetComponent<Player_Stat>().Get_P_State(4);
         Debug.Log("모자 스킬발사 히히");
@@ -21,6 +28,8 @@ public class H_Skill_01 : MonoBehaviour, Skill
     {
         GameObject Player = GameObject.Find("Player");
         Player.GetComponent<Player_Stat>().Set_P_State(4, reset_stat);
+
+        Destroy(Buffe_);
         Debug.Log("모자 스킬종료 희희");
     }
 }

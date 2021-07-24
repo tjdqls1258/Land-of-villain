017bacf8b55271505a8ba2a92282f74debc54804
20171119kim <skyksk981205@gmail.com>
2021-07-24 18:29:05 +0900

    2021.07.24(김성겸) 보스몬스터, Drop_Item, Drop_Money,Monster_Stats 일부 수정
    Drop_Money의 코드 수정
    (get_Coin메소드의 버스 수정)
    (돈이 떨어질 위치를 정할 Transform 매개변수 메소드 마다 추가.)
    --------------------------------------
    
    King_of_the_Dark 스크립트 제작(위치 = Assets/Monster/Boss_Monster_King_of_the_Dark)
    
    기본스텟.
    움직임(Seek사용), 패턴1(플레이어에게 이속 감속을 시켰다가 사망시 원상태로 돌림), 패턴2(하위 몬스터 출현<하위 몬스터가 필요해서 아직 구현 못함.>)
    사망(돈,아이템 드롭, 플레이어의 이동속도 복구).
    
    --------------------------------------
    
    Drop_Item도 Drop_Money처럼 void로 바로 생성해주는 코드 수정
    
    ----------------
    
    Monster_Stats의 die()함수 내부에
    
    Drop_Item의 수정에 따라 내용 수정.
    
    Drop_item_it = item_Drop.drop_Item(Monster_Drop_Tear);
    
            GameObject item = (GameObject)Resources.Load("Item/Item_Prefab/" + Drop_item_it);
            //GameObject item = (GameObject)Resources.Load("Item/Copper_Sword", typeof(GameObject));
            //리소스파일 속 item파일에 있는 Drop_item_it와 같은 이름을 가진 프리팹
            if (item != null) //있으면 소환 없으면 아무것도 안함
            {
                Instantiate(item, this.transform.position, Quaternion.identity);
            }
    
    위의 코드를 아래 코드처럼 변경.
    item_Drop.drop_Item(Monster_Drop_Tear,this.transform);
    
    ---------------------------------------------
    
    참고로 Monster_Stats의 die()에 Money_Drop과 Destroy()가 없어서 일단 King_of_the_dark의 내용은 일단 자체 제작함.
    
    단,King_of_the_dark에는 적이 데미지를 입는 것은 미구현(패턴과 스텟,플레이어 추적, 아이템과 돈 드랍만 구현.)
    
    추후에 Monster_Stats를 보안하고나서 다시 King_of_the_dark 스크립트를 수정할 예정.

--- Unity Project/Assets/Item/Drop_Money.cs
+++ Unity Project/Assets/Item/Drop_Money.cs
@@ -7,54 +7,54 @@ public class Drop_Money : MonoBehaviour
 
     int Round = 1;//라운드에 따라서 돈을 얻을 수 있는 금액 증가.
     //몬스터의 종류에 따라 돈을 떨구는 코드 (0:일반 몬스터,1:레어 몬스터, 2:보스 몬스터)
-    public void drop_Money(int get_Monster)
+    public void drop_Money(int get_Monster,Transform TS)
     {
         if (get_Monster == 0)
-        { get_Nomal_Monster(); }
+        { get_Nomal_Monster( TS); }
         else if (get_Monster == 1)
-        { get_Rare_Monster(); }
+        { get_Rare_Monster( TS); }
         else if (get_Monster == 2)
-        { get_Boss_Monster(); }
+        { get_Boss_Monster( TS); }
     }
     //일반 몬스터를 죽였을 시
     //일반 몬스터는 50%의 확률로 돈을 떨굼.
-    public void get_Nomal_Monster()
+    public void get_Nomal_Monster(Transform TS)
     {
-        get_Coin(50, 1, 50);
+        get_Coin(50, 1, 50,  TS);
     }
     //레어 몬스터를 죽였을 시
-    public void get_Rare_Monster()
-    { get_Coin(75, 50, 200); }
+    public void get_Rare_Monster(Transform TS)
+    { get_Coin(75, 50, 200,  TS); }
     //보스 몬스터를 죽였을 시
-    public void get_Boss_Monster()
-    { get_Coin(100, 200, 500); }
+    public void get_Boss_Monster(Transform TS)
+    { get_Coin(100, 200, 500,  TS); }
 
     //N1은 몬스터가 돈을 떨굴 확률, N2는 몬스터가 돈을 떨굴 금액.
-    public void get_Coin(int N1_Max,int N2_Min,int N2_Max)
+    public void get_Coin(int N1_Max,int N2_Min,int N2_Max, Transform TS)
     {
         int N1 = Random.Range(0, 100);
         if (N1 >= 100-N1_Max)
-        {   //1~50*현재 (라운드/10) 만큼의 금액을 얻음
-            int N2 = (Round/10) * Random.Range(N2_Min, N2_Max);
+        {   //1~50*현재 (라운드/10+1) 만큼의 금액을 얻음
+            int N2 = (Round/10+1) * Random.Range(N2_Min, N2_Max);
             //금화 현재 몬스터가 떨군 금액 /100 을 해서 떨굴 수 있는 금화의 갯수를 정해줌
             int G = N2 / 100;
             //은화는 몬스터가 떨군 금액에서 금화를 뺀 수치를 사용해 다시 10으로 나눈다. 
-            int S = (N2 - G) / 10;
+            int S = (N2 - (G*100)) / 10;
             //동화는 몬스터가 떨군 금액에서 금화,은화를 뺀 수치.
-            int C = (N2 - G - S);
+            int C = (N2 - (G*100) - (S*10));
 
             //아래는 동전들의 값 만큼 죽은 몬스터와 가까운 위치에 동전을 생성.
             //돈이 생성되는 구간은 몬스터가 죽은 자리 + (-1,1)사이의 값
             for (int i = 0; i < G; i++)
             {
-                Instantiate(Resources.Load("Money_Prefab/Gold_Coin"), new Vector2(transform.position.x+Random.Range(-1,1),transform.position.y + Random.Range(-1, 1)), Quaternion.identity);
+                Instantiate(Resources.Load("Money_Prefab/Gold_Coin"), new Vector2(TS.position.x+Random.Range(-1,1), TS.position.y + Random.Range(-1, 1)), Quaternion.identity);
             }
 
             for (int i = 0; i < S; i++)
-            { Instantiate(Resources.Load("Money_Prefab/Silver_Coin"), new Vector2(transform.position.x + Random.Range(-1, 1), transform.position.y + Random.Range(-1, 1)), Quaternion.identity); }
+            { Instantiate(Resources.Load("Money_Prefab/Silver_Coin"), new Vector2(TS.position.x + Random.Range(-1, 1), TS.position.y + Random.Range(-1, 1)), Quaternion.identity); }
 
             for (int i = 0; i < C; i++)
-            { Instantiate(Resources.Load("Money_Prefab/Copper_Coin"), new Vector2(transform.position.x + Random.Range(-1, 1), transform.position.y + Random.Range(-1, 1)), Quaternion.identity); }
+            { Instantiate(Resources.Load("Money_Prefab/Copper_Coin"), new Vector2(TS.position.x + Random.Range(-1, 1), TS.position.y + Random.Range(-1, 1)), Quaternion.identity); }
         }
     }
 }
---
+++ Unity Project/Assets/Monster/Boss_Monster_king_of_the_dark/king_of_the_dark.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class king_of_the_dark : MonoBehaviour
+{
+
+    string Name = "king_of_the_dark"; //이름
+    int MaxHP = 1000; //최대 체력
+    int HP = 0;//체력
+    //    int HP = 1000;//체력
+    int ATK =0;//공격력
+    int DEF = 20;//방어력
+    int AGI = 5;//이동 속도
+    int LUC = 0; //운(치명타)
+
+    //추척을 위한 FSM 변수들
+    private Rigidbody2D RB2D;//Rigidboy2D
+    private GameObject target;//타겟이 될 게임오브젝트를 가져옴.
+    private Vector2 VD;//자신의 백터 위치
+
+    //패턴 1의 플레이어의 AGI를 저장해둠.(패턴1)
+    int Save_P_AGI = 0;
+    //사망시 보스에 걸맞는 돈과 아이템을 떨구기 위한 Drop_Item 클래스를 가져옴.
+    Drop_Item DI = new Drop_Item();
+    Drop_Money DM=new Drop_Money();
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB2D = GetComponent<Rigidbody2D>();//Rigidbody2D 컴포넌트를 찾아서 가져옴
+        VD = Vector2.zero; //Vector2 초기화
+        target = GameObject.Find("Player");//타겟을 Player라는 이름을 가진 오브젝트로 선택.
+        //시작시 플레이어의 AGI를 가져옴.(패턴1)
+        Save_P_AGI = target.GetComponent<Player_Stat>().Get_P_State(4);
+        //시작시 플레이어의 AGI를 -5함(패턴1)
+        Pattern_One();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //플레이어의 HP<=0이면 실행.
+        Dead();
+        //플레이어 추적
+        Move();
+        //10초마다 하위의 적을 소환하는 패턴(패턴2)
+        Pattern_Two();
+
+
+    }
+    //필요한 것
+
+    //1.이동(플레이어 추척)
+    public void Move()
+    {
+
+        //플레이어 추적
+        VD = Seek(target.transform.position) * Time.deltaTime;
+        RB2D.velocity += VD;//Rigidbody2D의 Velocity값 변경
+    }
+    Vector2 Seek(Vector2 TargetPos)
+    {
+        Vector2 DesiredVelocity = TargetPos - RB2D.position;
+        DesiredVelocity.Normalize();
+        DesiredVelocity *= AGI;
+        return (DesiredVelocity - RB2D.velocity);
+    }
+
+
+
+    //2.패턴1(플레이어의 이동속도 5 감소) 한번만 실행되면 계속 유지되게끔. (원래는 90% 감소를 하려 했지만 플레이어의 스텟이 int인 관계로 -5를 함.)
+    public void Pattern_One()
+    {
+        target.GetComponent<Player_Stat>().Set_P_State(4, Save_P_AGI - 5);
+    }
+    //3.패턴2(하위 몬스터 10초 마다 소환)
+    public void Pattern_Two()
+    {   //10초마다 한번씩 소환.(일단 기본적으로 시작할 때 적을 5기 배치하는 것도 구상중.)
+        InvokeRepeating("Spawn_Enemy",10f, 10f);
+
+    }
+    //적을 소환하는 메소드.
+    public void Spawn_Enemy()
+    {
+        //하위 몬스터를 만들어야함.
+        //적의 위치애서 -1~1사이에 몬스터 생성.
+        //Instantiate(Resources.Load("하위 몬스터 프리팹의 위치"), new Vector2(transform.position.x+Random.Range(-1,1),transform.position.y + Random.Range(-1, 1)), Quaternion.identity);
+    }
+    //4.사망시 아이템 드롭 후 자체 파괴, 플레이어의 이동속도를 원상복구 해야함.
+    public void Dead()
+    {  if (HP <= 0)
+        {
+            //파괴
+            Destroy(gameObject);
+
+            //사망시 아이템을 떨굼. 
+            DI.drop_Item(1, transform);
+            //사망시 돈을 떨굼.
+            DM.drop_Money(2, transform);
+            //다시 플레이어의 AGI를 원상복구
+            target.GetComponent<Player_Stat>().Set_P_State(4, Save_P_AGI);
+
+        }
+
+    }
+
+}

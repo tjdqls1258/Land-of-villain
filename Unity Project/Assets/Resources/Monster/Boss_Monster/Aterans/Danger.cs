9bbdc0f54077c5719a6a908b776fc2a22a7c023a
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-08-03 18:01:13 +0900

    보스 패턴 간소화 후 데미지 처리 수정
    보스 패턴 간소화 후 데미지 처리 수정

--- Unity Project/Assets/Resources/Monster/Boss_Monster/Aterans/Danger.cs
+++ Unity Project/Assets/Resources/Monster/Boss_Monster/Aterans/Danger.cs
@@ -6,16 +6,19 @@ public class Danger : MonoBehaviour
 {
     GameObject Skills;
     Vector3 this_transform;
-    public void Patan_Ative(GameObject Skills)
+    int Damages;
+    public void Patan_Ative(GameObject Skills, int Damage)
     {
         GameObject Player = GameObject.Find("Player");
         GameObject this_gameObject = Instantiate(gameObject, Player.transform.position, Quaternion.identity);
         this_transform = this_gameObject.transform.position;
         this.Skills = Skills;
+        Damages = Damage;
         Invoke("Do_Skill",0.25f);
     }
     void Do_Skill()
     {
-        Instantiate(Skills, this_transform, Quaternion.identity);
+        GameObject Do_Skill =  Instantiate(Skills, this_transform, Quaternion.identity);
+        Do_Skill.GetComponent<Skill_damage>().Set_Damage(Damages);
     }
 }

8298bc4cb2a8efa21a6c4a5f0112995cea4e3b3d
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-08-08 18:52:12 +0900

    아이템 드랍 오류 수정
    아이템 드랍 오류 수정

--- Unity Project/Assets/Item/Get_Item/Item_List.cs
+++ Unity Project/Assets/Item/Get_Item/Item_List.cs
@@ -6,7 +6,7 @@ public class Item_List
 {
     //아이템을 늘리려면 동일한 배열의 수만큼 배열값을 할당해 줘야함. Copper_Sword2는 예시로 넣어둠
     //Normal 아이템 이름.(일반적인 아이템)
-    private string[,] N_Item = new string[,] { { "Copper_Sword", "Copper_Sword2" }, { "Copper_Armor", "NONE" }, { "Copper_Hat", "NONE" }, { "Copper_Ring", "NONE" } };
+    private string[,] N_Item = new string[,] { { "Copper_Sword", "NONE" }, { "Copper_Armor", "NONE" }, { "Copper_Hat", "NONE" }, { "Copper_Ring", "NONE" } };
     //Rare 아이템 이름.(좋은 아이템)
     private string[,] R_Item = new string[,] { { "Silver_Sword", "NONE" }, { "Silver_Armor", "NONE" }, { "Silver_Hat", "NONE" }, { "Silver_Ring", "NONE" } };
     //Epic 아이템 이름.(희귀한 아이템)

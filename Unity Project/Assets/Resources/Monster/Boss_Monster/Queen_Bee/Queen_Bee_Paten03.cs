e618c8185dca6574b9980f6ee1c3312ee2055943
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-08-10 11:21:30 +0900

    여왕벌 패턴 완성
    패턴 1,2,3,4 전부 클리어

---
+++ Unity Project/Assets/Resources/Monster/Boss_Monster/Queen_Bee/Queen_Bee_Paten03.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queen_Bee_Paten03 : MonoBehaviour
+{
+    public GameObject Bullte;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject Bulltes = Instantiate(Bullte, transform.position, Quaternion.identity);
+        Bullte.GetComponent<Skill_damage>().Set_Damage(GetComponent<Skill_damage>().Damage());
+    }
+
+}
---
+++ Unity Project/Assets/Set_Chiled.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Chiled : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Set_all_Bullte(int Damage)
+    {
+        Transform[] allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            if (child.name == transform.name)
+                continue;
+            child.GetComponent<Monster_Bullet>().Set_Damage(Damage);
+            Debug.Log(child.name);
+        }
+    }
+}

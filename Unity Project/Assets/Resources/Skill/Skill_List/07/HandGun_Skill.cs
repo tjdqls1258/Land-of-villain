579296bf2f0eb1cc20d15d7d4f4c8a74c599efc8
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-08-20 00:38:25 +0900

    아이템 추가
    길리슈트 핸드건 (스킬 아직 안넣음)

---
+++ Unity Project/Assets/Resources/Skill/Skill_List/07/HandGun_Skill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGun_Skill : MonoBehaviour, Skill
+{
+    public void Skill_Action()
+    {
+
+    }
+
+    public void Passive()
+    {
+
+    }
+    //중지시키는 함수
+    public void Stop_Passive()
+    {
+    }
+}

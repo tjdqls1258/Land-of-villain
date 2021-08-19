3b418521c6f1fc0bd21dede609e9f303d6a6430d
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-08-19 15:12:38 +0900

    마법의 지팡이 완성
    마법의 지팡이 완성

---
+++ Unity Project/Assets/Resources/Skill/Skill_List/06/Fire_Stafe_Skill_Bullte.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Stafe_Skill_Bullte : MonoBehaviour
+{
+    GameObject Player;
+    private void Awake()
+    {
+        Player = GameObject.Find("Player");
+        Player.GetComponent<SkillCooldown>().atkdelay = true;
+        Invoke("Set_false",3.0f);
+    }
+    void Update()
+    {
+        float angle = Mathf.Atan2(Joystick.inputDirection.y
+            , Joystick.inputDirection.x) * Mathf.Rad2Deg;
+
+        gameObject.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        gameObject.transform.position = Player.transform.position;
+    }
+    void Set_false()
+    {
+        Player.GetComponent<SkillCooldown>().atkdelay = false;
+    }
+}

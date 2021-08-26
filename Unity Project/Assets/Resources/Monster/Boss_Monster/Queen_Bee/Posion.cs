c43207b2b1e0a874ed8a1a084858f612bc3063dc
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-08-26 11:10:55 +0900

    패치 1.0.41
    패치 1.0.41
    쓸데없는 파일 삭제

--- Unity Project/Assets/Resources/Monster/Boss_Monster/Queen_Bee/Posion.cs
+++ Unity Project/Assets/Resources/Monster/Boss_Monster/Queen_Bee/Posion.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Posion : MonoBehaviour
 {
-    public int Posion_Damage;
+    public int Posion_Damages;
+    private void Awake()
+    {
+        Posion_Damages = Mathf.CeilToInt(GetComponent<Monster_Bullet>().Damage() * 0.1f);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            collision.GetComponent<Player_Debuff>().Poison_Debuff(Posion_Damage);
+            collision.GetComponent<Player_Debuff>().Poison_Debuff(Posion_Damages);
         }
     }
 }
--- Unity Project/Assets/SettingMap.cs
+++ Unity Project/Assets/SettingMap.cs
@@ -6,17 +6,20 @@ public class SettingMap : MonoBehaviour
 {
     Player_Stat player_state;
     public GameObject Cam_BGM;
-    
+    public AudioClip Sound2;
     public AudioClip Sound;
     // Start is called before the first frame update
     void Awake()
     {
         player_state = GameObject.Find("Player").GetComponent<Player_Stat>();
-        if (player_state.N_Stages >= 0)
+        if ((player_state.N_Stages % 36) >= 0)
         {
+            transform.Find("Stage02").gameObject.SetActive(false);
             transform.Find("Stage01").gameObject.SetActive(true);
+            Cam_BGM.GetComponent<AudioSource>().clip = Sound2;
+            Cam_BGM.GetComponent<AudioSource>().Play();
         }
-        if (player_state.N_Stages > 11)
+        if ((player_state.N_Stages % 36) > 11)
         {
             transform.Find("Stage01").gameObject.SetActive(false);
             transform.Find("Stage02").gameObject.SetActive(true);

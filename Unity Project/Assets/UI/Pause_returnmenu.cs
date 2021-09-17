87ad846cf2cdfced8065a7c21c334d671f4dfb44
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2021-09-17 19:13:42 +0900

    일시정지 수정

---
+++ Unity Project/Assets/UI/Pause_returnmenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_returnmenu : MonoBehaviour
+{
+    public GameObject Player;
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private GameObject ReturnToMenuUI;
+
+    public void returnyes()
+    {
+        GameManager.isPause = false;
+        SceneManager.LoadScene("Start_Secen");
+        Destroy(Player.gameObject);
+        pauseUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        Debug.Log("load start");
+    }
+
+    public void returnno()
+    {
+        ReturnToMenuUI.SetActive(false);
+    }
+}
